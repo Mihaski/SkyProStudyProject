2abf8cee0c0f6391e71067a84e72cf238b86490b
Mikey Zabrodin <mzabrodin94@gmail.com>
2026-06-05 22:37:25 +0700

    Добавил функцию sor_by_date

--- src/processing.py
+++ src/processing.py
@@ -8,3 +8,13 @@ def filter_by_state(list_dict: list[dict], state='EXECUTED') -> list[dict]:
             new_list.append(item)
 
     return new_list
+
+
+def sort_by_date(list_dict: list[dict], sort_by_decrease=True) -> list[dict]:
+    """ Функция возвращает список, отсортированный по дате"""
+
+    new_list = list_dict.copy()
+
+    new_list.sort(key=lambda x: x.get("date", ""), reverse=sort_by_decrease)
+
+    return new_list
