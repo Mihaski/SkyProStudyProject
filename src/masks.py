e84f071e1a6048930425d57e55e815b4a762410e
Mikey Zabrodin <mzabrodin94@gmail.com>
2026-06-09 14:25:21 +0700

    Тесты для преобразователя даты и для функции маскирования строк со счетом, картой и их номером

--- src/masks.py
+++ src/masks.py
@@ -7,7 +7,7 @@ def get_mask_card_number(card_number: str) -> str:
     """Hide card number"""
 
     if has_non_digit_except_spaces(card_number):
-        return "Ошибка: присутсвуют буквы"
+        return "Ошибка: присутствуют буквы"
 
     if card_number == "":
         return "Ошибка: пустая ввод"
@@ -24,7 +24,7 @@ def get_mask_account(account_number: str) -> str:
     """Hide account number"""
 
     if has_non_digit_except_spaces(account_number):
-        return "Ошибка: присутсвуют буквы"
+        return "Ошибка: присутствуют буквы"
 
     prep_account_number = re.sub(r'\D', '', str(account_number))
 
--- tests/test_masks.py
+++ tests/test_masks.py
@@ -6,7 +6,7 @@ from masks import get_mask_card_number, get_mask_account
 @pytest.mark.parametrize("card_number, expected", [
     ("7000 7922 8960 6361", "7000 79** **** 6361"),
     ("7000792289606361", "7000 79** **** 6361"),
-    ("70as7asd896063as", "Ошибка: присутсвуют буквы"),
+    ("70as7asd896063as", "Ошибка: присутствуют буквы"),
     ("7000792289606361123123123", "Ошибка: количество символов в номере больше или меньше"),
     ("", "Ошибка: пустая ввод"),
 ])
@@ -18,7 +18,7 @@ def test_get_mask_card_number(card_number, expected):
 
 @pytest.mark.parametrize("account_number, expected", [
     ("73654108430135874305", "**4305"),
-    ("736541sss3013587430a", "Ошибка: присутсвуют буквы"),
+    ("736541sss3013587430a", "Ошибка: присутствуют буквы"),
     ("", "Ошибка: номер счёта должен содержать минимум 4 цифры"),
 ])
 def test_get_mask_account(account_number, expected):
