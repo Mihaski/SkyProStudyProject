8a0856f853f6e97480988bcb784a6af119a420f3
Mikey Zabrodin <mzabrodin94@gmail.com>
2026-06-14 19:28:42 +0700

    func transaction_descriptions

--- src/generators.py
+++ src/generators.py
@@ -6,3 +6,10 @@ def filter_by_currency(list_dict_transactions: list[dict], currency: str) -> Gen
     for transaction in list_dict_transactions:
         if currency == transaction["currency"]["name"]:
             yield transaction
+
+
+def transaction_descriptions(list_dict_transactions: list[dict]) -> Generator[dict]:
+    """Выдает lazy описание транзакций"""
+    for transaction in list_dict_transactions:
+        yield transaction["description"]
+
