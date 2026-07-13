9a536bbe4319c1c8e3818f34e4ea524b2cff26e5
Mikey Zabrodin <mzabrodin94@gmail.com>
2026-07-14 00:08:32 +0700

    перенёс функцию

--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,8 @@ import json
 import os
 from json import JSONDecodeError
 
+from src.external_api import convert_currency
+
 
 def has_non_digit_except_spaces(text):
     return any(not (c.isdigit() or c.isspace()) for c in text)
@@ -21,7 +23,7 @@ def djecson_from_path(file_path: str) -> list[dict]:
 
     # Проверяем существование файла
     if not os.path.exists(file_path):
-        return ['check']
+        return []
 
     # Проверяем, что файл не пустой
     if os.path.getsize(file_path) == 0:
@@ -36,3 +38,24 @@ def djecson_from_path(file_path: str) -> list[dict]:
     except JSONDecodeError:
         # Возвращаем пустой список при любой ошибке
         return []
+
+
+def get_transaction_amount(transaction: dict) -> float:
+    """ Возвращает сумму из транзакции в рублях.
+        Транзакция - моделька, тип python объекта словарь. """
+
+    # Извлекаем сумму и валюту из транзакции
+    amount = transaction['operationAmount']['amount']
+    currency = transaction['operationAmount']['currency']['code']
+
+    # Если валюта не указана или это рубли, возвращаем сумму без изменений
+    if currency == 'RUB':
+        return float(amount)
+
+    # Для USD и EUR используем API для конвертации
+    if currency == 'USD' or currency == 'EUR':
+        prep = convert_currency(amount, currency, 'RUB')
+        return float(prep)
+
+    # Код ошибки будет todo
+    return -0.1
