510e5243756c502880a6cab52ddde3f5c353feca
Mikey Zabrodin <mzabrodin94@gmail.com>
2026-08-04 21:51:29 +0700

    функция поиска по описаниям банковских операций

--- src/processing.py
+++ src/processing.py
@@ -1,3 +1,6 @@
+import re
+
+
 def filter_by_state(list_dict: list[dict], state: str = "EXECUTED") -> list[dict]:
     """Функция возвращает список словарей, у которых ключ state соответствует указанному значению."""
 
@@ -18,3 +21,26 @@ def sort_by_date(list_dict: list[dict], sort_by_decrease: bool = True) -> list[d
     new_list.sort(key=lambda x: x.get("date", ""), reverse=sort_by_decrease)
 
     return new_list
+
+
+def process_bank_search(data: list[dict], search: str) -> list[dict]:
+    """ возвращает список словарей с банковскими операциями найденных по строке search"""
+    result = []
+
+    # Если список пустой или запрос пустой - возвращаем пустой список
+    if not data or not search:
+        return result
+
+    for item in data:
+        # Получаем описание транзакции (если ключа нет, то пустая строка)
+        description = item.get("description", "")
+
+        # Если описание пустое - пропускаем
+        if not description:
+            continue
+
+        # Ищем в описании
+        if re.search(search, description):
+            result.append(item)
+
+    return result
